9c000292d062cc5847cfe5533afc3949d9575f0e
0-Harsh-0 <harsh.the.programmer@@gmail.com>
2022-12-16 05:08:40 +0530

    write some comments

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-// const http = require('http')
+//For running it locally uncomment where u find - <UC> in code and Comment it where u find - <C>
+
+// Importing all required modules
+// const http = require('http')   //<UC>
 const fs = require('fs')
 const requests = require('requests')
 const path = require('path')
 
+//object of image names
 const ImageNames = {
     'Clouds': 'cloudy.png',
     'Rain': 'rain.png',
@@ -21,21 +25,25 @@ const ImageNames = {
     'Thunderstorm': 'thunderstorm.png',
 }
 
+//empty object to store image path according to weather status
 let statusIcons = {}
 
+//getting array of keys 
 let keys = Object.keys(ImageNames)
 for (let i = 0; i < keys.length; i++) {
-   
+    //resolving path 
     let imagePath = path.join(process.cwd(),'images',ImageNames[keys[i]])
+    // storing it into statusIcons
     statusIcons[keys[i]] = imagePath
 }
 
-
+//resolving path
 const filePath = path.resolve('home.html')
 
+//reading using synchronous function
 const home = fs.readFileSync(filePath , 'utf-8')
 
-
+//function which is used to replace particular strings in above read file which is home.html
 const replaceValueInHTMLFile = (temporaryValue, originalValue) => {
     let value = temporaryValue.replace("{%temperature%}", originalValue.main.temp)
     value = value.replace("{%minTemperature%}", originalValue.main.temp_min)
@@ -48,10 +56,10 @@ const replaceValueInHTMLFile = (temporaryValue, originalValue) => {
 
 
 //Main Function
-// const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {   //<UC>
 
-export default function handler(req, res) {
-
+export default function handler(req, res) { //<C>
+        //requesting to ipinfo for location using get
         requests('http://ipinfo.io')
             .on('data', function (data) {
                 let info = JSON.parse(data)
@@ -59,7 +67,9 @@ export default function handler(req, res) {
                 city = city.replace('ī', 'i')
                 city = city.replace('ā', 'a')
    
-                requests('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=cc176e73b0fc01aad0f8acb85855a04c&units=metric')
+                  //requesting to openweather api for weather data using get
+                  //here appid is a api key which you can get after registering on openweatherapi
+                requests('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=cc176e73b0fc01aad0f8acb85855a04c&units=metric') 
                     .on('data', function (chunk) {
                         const objectData = JSON.parse(chunk)
                         const realTimeData = replaceValueInHTMLFile(home, objectData)
@@ -80,9 +90,9 @@ export default function handler(req, res) {
                         res.end()
                     });
             })
-}
+} //<C>
 
-// })
+// })  //<UC>
 
-// server.listen(8000, "127.0.0.1")
+// server.listen(8000, "127.0.0.1") //<UC>
 
